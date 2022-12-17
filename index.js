8905eeb11199c861f18e11262651f045354509bc
SaAbdullah131 <cse.asabdullah@gmail.com>
2022-12-18 00:35:51 +0600

    js end

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const navbarEl=document.querySelector(".navbar");
+
+const bottomContainerEl=document.querySelector(".bottom-container");
+console.log("navbarEl.offsetHeight")
+console.log("bottomContainerEl.offsetTop");
+
+// console.log(navbarEl);
+
+window.addEventListener("scroll",()=>{
+    if(window.scrollY>bottomContainerEl.offsetTop -navbarEl.offsetHeight-50){
+        navbarEl.classList.add("active");
+    }else{
+        navbarEl.classList.remove("active");
+    }
+   //  console.log(window.scrollY);
+})
